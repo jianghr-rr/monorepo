fed3400cdedc5d5eb9002c968752de4f1c5efd15
姜浩然 <jianghaoran01@baidu.com>
2024-06-13 15:03:46 +0800

    loading data

--- apps/main-app/app/drinks/page.tsx
+++ apps/main-app/app/drinks/page.tsx
@@ -1,9 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a';
 
-const DrinksPage = async () => {
+const fetchDrinks = async () => {
+  // just for demo purposes
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   const response = await fetch(url);
-  const data = await response.json();
+  return await response.json();
+};
+const DrinksPage = async () => {
+  const data = await fetchDrinks();
   console.log(data);
   return (
     <div>
