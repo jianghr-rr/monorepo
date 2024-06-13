5009053695397dd82ea1cc1484d58619dbe4fa1b
姜浩然 <jianghaoran01@baidu.com>
2024-06-13 10:54:17 +0800

    feat: 添加homepage页面

--- apps/main-app/README.md
+++ apps/main-app/README.md
@@ -4,3 +4,15 @@
 npx create-next-app@latest appName
 ```
 ## Home Page 主页
+- page.js在应用程序文件夹的根目录中
+- React 组件（服务器组件）
+``` javaScript
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className='text-7xl'>HomePage</h1>
+    </div>
+  );
+};
+export default HomePage;
+```
--- apps/main-app/app/page.tsx
+++ apps/main-app/app/page.tsx
@@ -1,14 +1,23 @@
-import { AuthCard } from '~components/auth-card';
-import { SignOutButton } from '~components/sign-out-button';
-import { getSession } from '~lib/auth/session';
+// import { AuthCard } from '~components/auth-card';
+// import { SignOutButton } from '~components/sign-out-button';
+// import { getSession } from '~lib/auth/session';
 
-export default async function Home() {
-  const session = await getSession();
-  console.log('session::', session);
+// export default async function Home() {
+//   const session = await getSession();
+//   console.log('session::', session);
 
+//   return (
+//     <main className="flex min-h-screen items-center justify-center">
+//       {!session ? <AuthCard /> : <SignOutButton user={session?.user} />}
+//     </main>
+//   );
+// }
+
+const HomePage = () => {
   return (
-    <main className="flex min-h-screen items-center justify-center">
-      {!session ? <AuthCard /> : <SignOutButton user={session?.user} />}
-    </main>
+    <div>
+      <h1 className="text-7xl">HomePage</h1>
+    </div>
   );
-}
+};
+export default HomePage;
