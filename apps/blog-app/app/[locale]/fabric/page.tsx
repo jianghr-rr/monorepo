f58884ff0fa2835534d5ec94f03104a6461880fc
姜浩然 <jianghaoran01@baidu.com>
2024-06-27 13:53:49 +0800

    fix: try fix prerendering Error

--- apps/blog-app/app/[locale]/fabric/page.tsx
+++ apps/blog-app/app/[locale]/fabric/page.tsx
@@ -1,16 +1,18 @@
 'use client';
-
+import { Suspense } from 'react';
 import { useTranslation } from 'react-i18next';
 
 export default function FabricPage() {
   const { t } = useTranslation();
 
   return (
-    <div className="blog-container">
-      <h1 className="blog-h1">{t('title')}</h1>
-      <p>
-        https://www.youtube.com/watch?v=-_MGT_4jBms&list=PL-gIJFyHJjykXg776HNz3H7XXzBMSu5mL&index=5
-      </p>
-    </div>
+    <Suspense fallback={null}>
+      <div className="blog-container">
+        <h1 className="blog-h1">{t('title')}</h1>
+        <p>
+          https://www.youtube.com/watch?v=-_MGT_4jBms&list=PL-gIJFyHJjykXg776HNz3H7XXzBMSu5mL&index=5
+        </p>
+      </div>
+    </Suspense>
   );
 }
