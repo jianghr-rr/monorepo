bd829582248bbf7d6b4425ba28f9c53773ca3e4e
姜浩然 <jianghaoran01@baidu.com>
2024-10-08 19:28:08 +0800

    feat: 添加preload prefetch设计模式

---
+++ apps/blog-app/app/[locale]/patterns/import-on-visibility/example-2.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable jsx-a11y/media-has-caption */
+/* eslint-disable @typescript-eslint/prefer-optional-chain */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { useRef, useEffect } from 'react';
+
+const Example2 = () => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (videoRef && videoRef.current) {
+      // @ts-ignore
+      videoRef.current.play();
+    }
+  }, [videoRef]);
+
+  return (
+    <video ref={videoRef} width="1280" height="720" controls loop>
+      <source
+        src="/videos/patterns/visibility-import/import-on-visibility.webm"
+        type="video/webm"
+      />
+      Your browser does not support the video tag.
+    </video>
+  );
+};
+
+export default Example2;
+export { Example2 };
